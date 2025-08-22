8e085ebe60c1beeefd770b0346084c58009bfb37
Pedro U <pedroescapalete@gmail.com>
2025-08-22 14:51:34 -0300

    server iniciado com princesas/id

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import princesas from "./src/data/princesas.js";
+
+const app = express();
+const serverPorter = 3002;
+
+app.get ("/", (req, res) => {
+    res.send("Bem-vindos(as) ao reino mágico das Princesas Disney! ✨👑");
+});
+
+
+app.get ("/princesas", (req, res) => {
+    res.json(princesas);
+});
+
+app.get ("/princesas/id", (req, res) => {
+    let id = req.params.id;
+
+    id = parseInt(id);
+
+    const princesa = princesas.find(p => p.id === id);
+
+    if (princesa) {
+        res.json({
+            success: true,
+            message: `princesa ${princesa.nome} encontrada! ⚡`,
+            data: princesa
+        });
+    } else {
+        
+        res.status(404).json({
+            success: false,
+            error: "princesa não encontrada 😕",
+            message: `Nenhuma princesa com ID ${id} foi encontrada`,
+        });
+    }
+});
+
+
+app.listen(serverPorter, () => {
+    console.log(`Servidor funcionando na porta http://localhost:${serverPorter}`)
+});
